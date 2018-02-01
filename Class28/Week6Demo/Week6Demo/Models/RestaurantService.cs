101fd4aabc374c5c43324ab1bebd978017079e30
Iverson, Amanda <amanda@codefellows.com>
2018-01-31 17:10:09 -0800

    Class28 Code Demo

---
+++ Class28/Week6Demo/Week6Demo/Models/RestaurantService.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Week6Demo.Data;
+
+namespace Week6Demo.Models
+{
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly RestaurantDbContext _context;
+
+
+        public RestaurantService()
+        {
+            var options = new DbContextOptionsBuilder<RestaurantDbContext>()
+                .UseInMemoryDatabase("MyRestaurantFinder")
+                .Options;
+            _context = new RestaurantDbContext(options);
+                
+        }
+        public RestaurantService(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+        public async Task DeleteAsync(int id)
+        {
+           _context.Restaurant.Remove(new Restaurant { ID = id });
+            await _context.SaveChangesAsync();
+        }
+
+        public Restaurant Find(int id)
+        {
+            return _context.Restaurant.FirstOrDefault(x => x.ID == id);
+        }
+
+        public Task<Restaurant> FindAsync(int id)
+        {
+            return _context.Restaurant.FirstOrDefaultAsync(x => x.ID == id);
+
+        }
+
+
+        public IQueryable<Restaurant> GetAll(int? count = null, int? page = null)
+        {
+            var actualCount = count.GetValueOrDefault(10);
+
+            return _context.Restaurant
+                    .Skip(actualCount * page.GetValueOrDefault(0))
+                    .Take(actualCount);
+        }
+
+        public Task<Restaurant[]> GetAllAsync(int? count = null, int? page = null)
+        {
+            return GetAll(count, page).ToArrayAsync();
+        }
+
+
+        public async Task SaveAsync(Restaurant restaurant)
+        {
+            var doesItExist = restaurant.ID == default(int) ;
+
+            _context.Entry(restaurant).State = doesItExist ? EntityState.Added : EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Class28/Week6Demo/Week6Demo/Pages/Admin/AddEditRestaurant.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Week6Demo.Models;
+
+namespace Week6Demo.Pages.Admin
+{
+    public class AddEditRestaurantModel : PageModel
+    {
+        [FromRoute]
+        public int? ID { get; set; }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        private readonly IRestaurantService restaurantService;
+
+        public AddEditRestaurantModel(IRestaurantService rs)
+        {
+            restaurantService = rs;
+        }
+        public async Task OnGet()
+        {
+            Restaurant = await restaurantService.FindAsync(ID.GetValueOrDefault()) ?? new Restaurant();
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            Restaurant rest = await restaurantService.FindAsync(ID.GetValueOrDefault()) ?? new Restaurant();
+            rest.Name = Restaurant.Name;
+            rest.Description = Restaurant.Description;
+            rest.MenuItems = Restaurant.MenuItems;
+            rest.StarRating = Restaurant.StarRating;
+
+            await restaurantService.SaveAsync(rest);
+
+            return RedirectToPage("/Restaurant", new { id = rest.ID });
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await restaurantService.DeleteAsync(ID.Value);
+            return RedirectToPage("/Index");
+
+        }
+    }
+}
