09a0f241e570981450fcb04448fe8f5df14eb840
Iverson, Amanda <amanda@codefellows.com>
2018-02-08 20:31:58 -0800

    Add Class Demo

---
+++ Class34/Week7Demo/Week7Demo/Controllers/PageController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Week7Demo.Models;
+
+namespace Week7Demo.Controllers
+{
+    [Authorize(Policy = "MinimumAge")]
+  //  [Authorize(Policy = "Admin Only")]
+    //[Authorize(Roles = "Member")]
+
+    public class PageController : Controller
+    {
+        private readonly Week7DemoContext _context;
+
+        public PageController(Week7DemoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Page
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Page.ToListAsync());
+        }
+
+        // GET: Page/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Page
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // GET: Page/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Page/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Title,Body,IsPublished")] Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(page);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(page);
+        }
+
+        // GET: Page/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Page.SingleOrDefaultAsync(m => m.ID == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+            return View(page);
+        }
+
+        // POST: Page/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Title,Body,IsPublished")] Page page)
+        {
+            if (id != page.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(page);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PageExists(page.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(page);
+        }
+
+        // GET: Page/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var page = await _context.Page
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (page == null)
+            {
+                return NotFound();
+            }
+
+            return View(page);
+        }
+
+        // POST: Page/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var page = await _context.Page.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Page.Remove(page);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PageExists(int id)
+        {
+            return _context.Page.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Class34/Week7Demo/Week7Demo/Models/Page.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week7Demo.Models
+{
+    public class Page
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public bool IsPublished { get; set; }
+    }
+}
